2d26e8b73e86798ae6eb8c7e86cce186d63e0945
Gayrat <gena68@inbox.ru>
2020-07-24 00:58:52 +0300

    рабочая конфигурация CSS modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 // const isDev = process.env.NODE_ENV === 'development';
 const isDev = true;
@@ -10,35 +10,48 @@ const isProd = !isDev;
 const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
 
 module.exports = {
-  entry: "./src/index.tsx",
-  devtool: "source-map",
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-  },
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)x?$/,
-        loader: require.resolve("babel-loader"),
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['css-loader'], options: { modules: true }
-      },
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                loader: require.resolve("babel-loader"),
+                exclude: /node_modules/,
+            },
+            {
+
+                // css-loader - преобразовывает CSS - в модуль JavaScript.
+                // (Входные данные: CSS, Выходные данные: JavaScript)
+                // style-loader - внедряет CSS, экспортируемый модулем JavaScript, в тег style
+
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    }
+                ]
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new CleanWebpackPlugin(),
-  ],
 };
