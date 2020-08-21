7343d645cc1db49626c0fb3087f0aef93433dbd0
Ulibka <gena68@inbox.ru>
2020-08-21 14:18:01 +0300

    HOC withOnChangeValue работает

--- src/HOC/withOnChangeValue.stories.tsx
+++ src/HOC/withOnChangeValue.stories.tsx
@@ -10,11 +10,13 @@ export default {
 };
 
 // это тип компонент
-const CompComponent = () => <input type="text" defaultValue="txt" />;
-console.log("CompComponent", CompComponent);
-// это тип React.element он описывает как будет идти рендер
+const CompComponent = (props: any) => (
+  <input type="text" defaultValue="txt" {...props} />
+);
+// console.log("CompComponent", CompComponent);
+// это тип React.element он описывает как будет идти рендер - его в обертку передавать нельзя
 const CompElement = <input type="text" defaultValue="txt" />;
-console.log("CompElement", CompElement);
+// console.log("CompElement", CompElement);
 
 // Элемент нельзя передавать туда где нужен компонент - будет ошибка статья:
 // https://reactjs.org/blog/2015/12/18/react-components-elements-and-instances.html
@@ -28,4 +30,11 @@ const WW = withOnChangeValue(CompComponent);
 // если вызвать так - то обертка сраобтает
 // const WW = withOnChangeValue("input");
 
-export const with_On_ChangeValue = () => [<WW key={1} />];
+export const with_On_ChangeValue = () => [
+  <WW
+    key={1}
+    onChangeValue={(e: string) => {
+      console.log("onChangeValue: ", e);
+    }}
+  />,
+];
--- src/HOC/withOnChangeValue.tsx
+++ src/HOC/withOnChangeValue.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
+interface withOnChangeValuePropsInternal
+  extends React.ComponentPropsWithoutRef<"input"> {
+  onChangeValue?: (e: string) => void;
+}
+
 export const withOnChangeValue = (Component: React.ComponentType) => {
   return function InternalFunc(propsInternal: any) {
-    function ChangeHandler(e: React.ChangeEvent) {
-      console.log("функция из обертки");
+    function ChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+      if (propsInternal.onChangeValue) {
+        e.preventDefault();
+        propsInternal.onChangeValue(e.target.value);
+      } else console.log("база : ", e.target.value);
     }
 
-    console.log(Component);
-
-    return <Component onChange={ChangeHandler} {...propsInternal} />;
+    console.log(propsInternal);
+    return <Component {...propsInternal} onChange={ChangeHandler} />;
   };
 };
