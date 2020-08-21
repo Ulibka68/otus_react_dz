414c6c354272141688da8cf2a806c982a994e868
Ulibka <gena68@inbox.ru>
2020-08-21 15:56:50 +0300

    роутер1

---
+++ src/component/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>Начальный экран приложения </h1>
+      <p>Информация о приложении</p>
+    </div>
+  );
+}
--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,21 +1,57 @@
 import React from "react";
 import FlickrContainer from "./FlickrContainer";
 import Basic from "./FormParams";
+import About from "./About";
 
-export default class App extends React.Component<any, any> {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  withRouter,
+} from "react-router-dom";
+
+// параметры определения пути
+// https://github.com/pillarjs/path-to-regexp#parameters
+
+class App extends React.Component<any, any> {
   constructor(props?: any) {
     super(props);
   }
 
   render() {
     return (
-      <div>
+      <Router>
         <div>
-          <h1>Урок9 формы</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/forms">Формы</Link>
+            </li>
+          </ul>
         </div>
-        {/*<FlickrContainer />*/}
-        <Basic />
-      </div>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/forms">
+            <div>
+              <h1>Урок9 формы</h1>
+            </div>
+            {/*<FlickrContainer />*/}
+            <Basic />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
+
+// export default withRouter(App);
+export default App;
