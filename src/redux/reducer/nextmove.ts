01a14184471577ebf0e61c5b5d7c4611e61e1c99
Ulibka <gena68@inbox.ru>
2020-09-14 15:38:04 +0300

    extraReducer разобрал

--- src/redux/reducer/nextmove.ts
+++ src/redux/reducer/nextmove.ts
@@ -1,4 +1,4 @@
-import { createSlice,  SliceCaseReducers} from "@reduxjs/toolkit";
+import {createAction, createSlice, SliceCaseReducers} from "@reduxjs/toolkit";
 
 type nextMoveState = "x" | "o";
 
@@ -6,6 +6,10 @@ const defaultStatenextMove: nextMoveState = "x";
 
 
 type nextMoveSliceReducerOptionType = SliceCaseReducers<nextMoveState>;
+
+const incrementBy = createAction('incrementBy');
+// extraReducers
+// лучше всего строить черз builder
 
 export const nextMove = createSlice<nextMoveState,nextMoveSliceReducerOptionType>({
     name: 'nextMove',
@@ -17,10 +21,39 @@ export const nextMove = createSlice<nextMoveState,nextMoveSliceReducerOptionType
         xMove(state,action) {
             return "o";
         },
+    },
+    extraReducers: {
+        incrementByReducer: (state, action) => {
+            if ( incrementBy.match(action) ) {
+                return "x"
+            }
+        }
     }
+
+    /*
+    extraReducers: builder =>
+    {
+        builder.addCase(incrementBy,
+                (state, action) => {return "x"}
+                )
+    }
+
+     */
 });
 
+
+/*type AppActions = typeof appActions;
+type AppActionsType = AppActions[keyof AppActions]['type'];
+const x1:AppActionsType = 'a1';*/
+
 console.log('nextMove.actions : ', nextMove.actions);
+console.log(nextMove.actions.oMove.type);
+
+
+type AppActions = typeof nextMove.actions;
+type AppActionsType = AppActions[keyof AppActions]['type'];
+
+const a : AppActionsType = 'nextMove';
 
 /*
 type GameFieldState = string[][];
