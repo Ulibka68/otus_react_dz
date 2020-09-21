c86e6b13e911e3c16a672bb86b03a97aab234125
Ulibka <gena68@inbox.ru>
2020-09-21 17:58:37 +0300

    эксперименты с CSS

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,12 @@ module.exports = {
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-react",
     "@babel/preset-typescript",
+    "@emotion/babel-preset-css-prop",
   ],
   plugins: ["@babel/plugin-proposal-class-properties"],
   env: {
     production: {
-      plugins: ["emotion"],
+      plugins: ["emotion", "@babel/plugin-transform-react-jsx"],
     },
     development: {
       plugins: [["emotion", { sourceMap: true }]],
