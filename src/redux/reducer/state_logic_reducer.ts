c50fdec7a30dedd13cb23e72f8ada331f20dd471
Gayrat <gena68@inbox.ru>
2020-10-04 11:52:33 +0300

    eslintrc поправил

--- src/redux/reducer/state_logic_reducer.ts
+++ src/redux/reducer/state_logic_reducer.ts
@@ -1,36 +1,47 @@
-import {createSlice, PayloadAction, SliceCaseReducers} from "@reduxjs/toolkit";
+import {
+  createSlice,
+  PayloadAction,
+  SliceCaseReducers,
+} from "@reduxjs/toolkit";
 
 interface lifeStateType {
-    state: number[][];
-    neighbors: number[][];
-    sizex: number;
-    sizey: number;
-
+  state: number[][];
+  neighbors: number[][];
+  sizex: number;
+  sizey: number;
 }
 
 const defaultlifeState: lifeStateType = {
-    state: [],
-    neighbors: [],
-    sizex: 0,
-    sizey: 0
+  state: [],
+  neighbors: [],
+  sizex: 0,
+  sizey: 0,
 };
 
-export const nextMove = createSlice<lifeStateType, SliceCaseReducers<lifeStateType>>({
-        name: 'lifeState',
-        initialState: defaultlifeState,
-        reducers: {
-            initState(state, action: PayloadAction<{
-                sizex: number,
-                sizey: number
-            }>) {
-                return {
-                    state: Array.from(Array(action.payload.sizey), () => new Array(action.payload.sizex).fill(0)),
-                    neighbors: Array.from(Array(action.payload.sizey), () => new Array(action.payload.sizex).fill(0)),
-                    sizex: action.payload.sizex,
-                    sizey: action.payload.sizey
-                }
-            }
-        }
-    }
-);
-
+export const nextMove = createSlice<
+  lifeStateType,
+  SliceCaseReducers<lifeStateType>
+>({
+  name: "lifeState",
+  initialState: defaultlifeState,
+  reducers: {
+    initState(
+      state,
+      action: PayloadAction<{
+        sizex: number;
+        sizey: number;
+      }>
+    ) {
+      return {
+        state: Array.from(Array(action.payload.sizey), () =>
+          new Array(action.payload.sizex).fill(0)
+        ),
+        neighbors: Array.from(Array(action.payload.sizey), () =>
+          new Array(action.payload.sizex).fill(0)
+        ),
+        sizex: action.payload.sizex,
+        sizey: action.payload.sizey,
+      };
+    },
+  },
+});
